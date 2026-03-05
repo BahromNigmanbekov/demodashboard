560cda97504b8c5adfa6eefbd663453c14a7eb78
BahromNigmanbekov <bahromnigmanbekov@gmail.com>
2026-03-05 16:33:36 +0500

    first commst

--- src/components/forms/Forms.tsx
+++ src/components/forms/Forms.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import "./Forms.css"
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
